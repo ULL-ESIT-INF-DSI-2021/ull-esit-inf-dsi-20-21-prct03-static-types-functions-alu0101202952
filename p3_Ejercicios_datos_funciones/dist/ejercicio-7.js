4d4e9b2697f887ffd8a243b8babd9216ba18b97a
Andrea Calero Caro <alu0101202952@ull.edu.es>
2021-03-05 21:50:18 +0000

    Ejercicio 9 correcto

---
+++ p3_Ejercicios_datos_funciones/dist/ejercicio-7.js
@@ -0,0 +1,21 @@
+//EJERCICIO 7
+/*
+function changeDigit(cifra: number){
+  var change= cifra.toString(); //trabajamos con strings
+  var auxstr = [];
+  var valoresAceptados = /^[0-9]+$/;
+  var valminimo = [0];
+  for(var i = 0; i < change.length; i++){
+    var character = change.charAt(i);
+    var auxnumber = parseInt(character);
+    auxstr.push(character);
+    console.log(auxstr);
+    if(auxstr[i] < auxstr[i+1]){
+
+    }
+  }
+}
+
+var cifrain = 12;
+console.log(`${changeDigit(12)}`);
+*/ 
