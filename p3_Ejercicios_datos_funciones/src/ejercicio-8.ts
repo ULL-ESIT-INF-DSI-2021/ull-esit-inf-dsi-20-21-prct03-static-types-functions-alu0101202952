d84c7f89d95e0c84c7657169b01b00368ebf7c2c
Andrea Calero Caro <alu0101202952@ull.edu.es>
2021-03-07 15:45:36 +0000

    Typedoc ejercicio 7 y 8 correcto

--- p3_Ejercicios_datos_funciones/src/ejercicio-8.ts
+++ p3_Ejercicios_datos_funciones/src/ejercicio-8.ts
@@ -1,5 +1,13 @@
-//EJERCICIO 8
-/*
+/**
+ * ### EJERCICIO 8:
+ * #### ¿En qué consiste?
+ * A partir de cadenas de texto que representan direcciones IPv4, la función recibe como parámetros dos cadenas y devuelve un valor numérico que representa el número de IPs disponibles en el rango correspondiente.
+ * @param ips1 Consiste en la cadena con la IP de del rango mínimo
+ * @param ips2 Consiste en la cadena con la IP de del rango mayor 
+ * @return `resultado` El rango que sale de la diferencia entre la `ips2`(ip con rango mayor) y `ips1`(ip con rango menor)
+ * 
+ */
+
 function ipsInRange(ips1: string, ips2: string){
   var addr1 = parseInt(ips1);
   var addr2 = parseInt(ips2);
@@ -22,8 +30,6 @@ function ipsInRange(ips1: string, ips2: string){
       str2 += str2 + continu2;
     }
   }
-
-  // @return la ip en binario
   var ip1_binario = addr1.toString(2);
   console.log(`Número en binario IPs1 ("${ips1}"): ${ip1_binario}`);
   var ip2_binario = addr2.toString(2);
@@ -37,7 +43,13 @@ function ipsInRange(ips1: string, ips2: string){
 
 }
 
+/**
+ * ```typescript
+ * ips_1 = "10.0.0.0";
+ * ips_2 = "10.0.0.50";
+ * @return `Rango == 50`
+ * ```
+ */
 let ips_1 = "10.0.0.0";
 let ips_2 = "10.0.0.50";
 console.log(ipsInRange(ips_1, ips_2));
-*/
